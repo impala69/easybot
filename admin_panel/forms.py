a523b34200c12b15fd67e030c2fba7658f1919aa
mhdi01 <mhdi.gh75@gmail.com>
2017-08-12 15:58:51 +0430

    changes done

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class AddProductForm(forms.Form):
-    category_name = forms.CharField(label='Category')
-    product_name = forms.CharField(label='Product')
-    product_text = forms.CharField(label='ProductText')
+    category_name = forms.CharField(label='Category' , max_length=30)
+    product_name = forms.CharField(label='Product' , max_length=30)
+    product_text = forms.CharField(label='ProductText' , max_length=60)
     product_price = forms.IntegerField(label='ProductPrice')
-    product_image = forms.CharField(label='ProductImage')
+    product_image = forms.CharField(label='ProductImage' , max_length=30)
