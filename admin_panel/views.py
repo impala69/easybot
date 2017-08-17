ca7d0947aac706f156be84502f5332b1e945898b
mhdi01 <mhdi.gh75@gmail.com>
2017-08-17 17:19:46 +0430

    adding completed

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -3,46 +3,49 @@ from __future__ import unicode_literals
 import sys
 from django.shortcuts import render
 from .forms import AddProductForm
-from django.views.decorators.csrf import csrf_protect
+
 from django.http import HttpResponseRedirect
-sys.path.insert(0, '/E:/final/easybot/easybot/models')
-import models
+from easybot import models
 from django.db import IntegrityError
 from django.shortcuts import render_to_response
 
-@csrf_protect
+
 def adding(request):
-    print(2)
     if request.method == 'POST' :
-        print(3)
-        print models
+        print models.Customer
         adding_form = AddProductForm(request.POST)
+        print(adding_form)
         if adding_form.is_valid():
             product_name = adding_form.cleaned_data['product_name']
             text = adding_form.cleaned_data['product_text']
             image = adding_form.cleaned_data['product_image']
             price = adding_form.cleaned_data['product_price']
-            print(text)
+            print
             try:
                 new_product = models.Product(product_name=product_name,text=text,image=image,price=price)
+                print(new_product.text)
                 new_product.save()
                 print new_product
 
             except:
-                pass
+                print(5)
 
 
             return render_to_response("blank.html")
 
+        else:
+            return render_to_response("failed.html")
+
 
     return render_to_response("blank.html")
 
-@csrf_protect
+
 def showing(request):
     if request.method == 'POST':
         models_result = models.Product.objects.all()
 
-@csrf_protect
+
+
 def success(request):
     return render(request, "admin_panel/blank.html")
 
--- testak/settings.py
+++ testak/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'nwtiez4j(zc+r-#1p48msj@x^dsg@zvqt23k81=8ydfo)_h(7p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -46,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
