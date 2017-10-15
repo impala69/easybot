4a1936403f8efb4ab04a3d744317b2e007a5ec12
mhdi01 <mhdi.gh75@gmail.com>
2017-10-15 14:08:12 +0330

    admin panel changed

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -5,6 +5,15 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.adding),
-    url(r'^adding/$', views.adding),
-    url(r'^showing',views.showing)
+    url(r'^adding', views.adding),
+    url(r'^showing',views.showing),
+    url(r'^enteghadat',views.enteghadat),
+    url(r'^category', views.category),
+    url(r'^comments',views.comments),
+    url(r'^adding/adding',views.adding),
+    url(r'^adding/showing', views.showing),
+    url(r'^adding/enteghadat', views.enteghadat),
+    url(r'^adding/category', views.category),
+    url(r'^adding/comments', views.comments),
+
     ]
