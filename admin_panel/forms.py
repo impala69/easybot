5a62fa35318d3a58426bf12379e56f135e74f9d9
impala69 <imanshafiei@ymail.com>
2017-11-07 11:43:18 +0330

    Advertise admin panle Add delete show all DOne and order bug fixes and inproved

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
+
 class AddProductForm(forms.Form):
-    category_name = forms.CharField(label='Category' , max_length=30)
-    product_name = forms.CharField(label='Product' , max_length=30)
-    product_text = forms.CharField(label='ProductText' , max_length=60)
+    category_name = forms.CharField(label='Category', max_length=30)
+    product_name = forms.CharField(label='Product', max_length=30)
+    product_text = forms.CharField(label='ProductText', max_length=60)
     product_price = forms.IntegerField(label='ProductPrice')
-    product_image = forms.CharField(label='ProductImage' , max_length=30)
+    product_image = forms.CharField(label='ProductImage', max_length=30)
+
 
 class EditProductForm(forms.Form):
     product_image = forms.CharField(max_length=30, label='ProductImage')
@@ -13,7 +15,14 @@ class EditProductForm(forms.Form):
     product_text = forms.CharField(label='ProductText', max_length=30)
     product_name = forms.CharField(label="Product", max_length=30)
     product_number = forms.IntegerField(label="ProductNumber")
-    category_name = forms.CharField(label='Category' , max_length=30)
+    category_name = forms.CharField(label='Category', max_length=30)
+
 
 class AddCategoryForm(forms.Form):
-    category_name = forms.CharField(label="Category" , max_length=30)
+    category_name = forms.CharField(label="Category", max_length=30)
+
+
+class AddAdvertiseForm(forms.Form):
+    advertise_title = forms.CharField(label="Advertise Title", max_length=300)
+    advertise_text = forms.CharField(widget=forms.Textarea)
+    advertise_image = forms.ImageField()
--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,16 +1,14 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.adding),
     url(r'^adding', views.adding),
-    url(r'^showing',views.showing),
-    url(r'^enteghadat',views.enteghadat),
+    url(r'^showing', views.showing),
+    url(r'^enteghadat', views.enteghadat),
     url(r'^category', views.category),
-    url(r'^comments',views.comments),
-    url(r'^adding',views.adding),
+    url(r'^comments', views.comments),
+    url(r'^adding', views.adding),
     url(r'^adding/showing/', views.showing),
     url(r'^add_feed_cat', views.add_naghd),
     url(r'^adding/category/', views.category),
@@ -22,17 +20,19 @@ urlpatterns = [
     url('orders', views.orders),
     url(r'^editDescription', views.editDescription),
     url(r'^deletecomment', views.deletecomment),
-    url(r'^edit',views.edit),
-    url(r'^edit/showing',views.showing),
-    url(r'^delete',views.delete),
-    url(r'^del_cat',views.del_cat),
-    url(r'^add_cat',views.add_cat),
-    url(r'^del_cat',views.del_cat),
-    url(r'^add_cat',views.add_cat),
-    url(r'^cm_del',views.cm_del),
-    url(r'^show_naghd_cat',views.show_naghd_cat),
-    url(r'^add_naghd',views.add_naghd),
-    url(r'^del_naghd',views.del_naghd),
-    url(r'^ed_cat',views.ed_cat),
-
+    url(r'^edit', views.edit),
+    url(r'^edit/showing', views.showing),
+    url(r'^delete', views.delete),
+    url(r'^del_cat', views.del_cat),
+    url(r'^add_cat', views.add_cat),
+    url(r'^del_cat', views.del_cat),
+    url(r'^add_cat', views.add_cat),
+    url(r'^cm_del', views.cm_del),
+    url(r'^show_naghd_cat', views.show_naghd_cat),
+    url(r'^add_naghd', views.add_naghd),
+    url(r'^del_naghd', views.del_naghd),
+    url(r'^ed_cat', views.ed_cat),
+    url(r'^add_advertise', views.add_advertise),
+    url(r'^advertise', views.advertise),
+    url(r'^del_ad', views.del_ad),
     ]
